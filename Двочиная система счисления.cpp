b1fd2a8f19171e2fbf02a351a1beb2ebd31ef16a
SunlessValera <84783256+SunlessValera@users.noreply.github.com>
2021-10-10 12:47:11 +0300

    Add files via upload

---
+++ Двочиная система счисления.cpp
@@ -0,0 +1,53 @@
+﻿// Двочиная система счисления.cpp 
+//
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+void bin(int b) {
+    int n = 0;
+    int k, s = 0;
+    while (b) {
+        k = b % 2;
+        b /= 2;
+        n += k * pow(10, s);
+        s++;
+    }
+    cout << n<<endl;
+
+}
+
+int main()
+{
+    int c, z = 0;
+
+    cin >> c;
+    bin(c);
+}
+
+
+
+
+
+// Перевод двочиной системы счисления без функции
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n = 0;
+    int k, s = 0;
+    while (b) {
+        k = b % 2;
+        b /= 2;
+        n += k * pow(10, s);
+        s++;
+    }
+    cout << n << endl;
+
+ 
+}
+
+
